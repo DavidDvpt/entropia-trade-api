a3b1d944793c4ede4940d319328ea740108149cd
David Mosca <david.mosca69@gmail.com>
2021-03-26 18:48:38 +0100

    add beforeEach and afterEach for tests works. install faker

--- src/tests/type_item.test.js
+++ src/tests/type_item.test.js
@@ -1,12 +1,32 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('../app');
+
+let server;
+let agent;
+
+beforeEach((done) => {
+  server = app.listen(4000, (err) => {
+    if (err) return done(err);
+
+    agent = request.agent(server); // since the application is already listening, it should use the allocated port
+    done();
+    return true;
+  });
+});
+
+afterEach((done) => {
+  return server && server.close(done);
+});
 
 describe('Get Endpints', () => {
   it('should creat an type_item', async () => {
-    const res = await request(app)
-      .post('/api/v1/type_Item')
-      .send({ id: 'a', name: 'testname', isActive: true });
-    expect(res.statusCode).equal(201);
+    // await request(app)
+    const res = await agent
+      .post('/api/v1/type_item')
+      .send({ name: 'testname', isActive: true });
+    expect(res.status).toBe(201);
+    expect(res.status).toEqual(201);
+
     // expect(res.body).toHaveProperty('post');
   });
 });
